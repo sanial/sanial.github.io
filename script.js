aa2c4619331214ad9b5bcde1ed6eb8c02b1ef1e0
sanial <sanial369@gmail.com>
2023-08-27 18:23:18 -0400

    #82723 new look

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slide");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  slides[slideIndex-1].style.display = "block";
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
 }
