fe6b3ba418364b945544959858ba4b19a33c6979
sanial <sanial369@gmail.com>
2023-09-01 21:27:26 -0400

    #9123-2126 proj-extra's icon-menu2 fixes

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    } 
-  });
-}
+$('[data-toggle="tooltip"]').tooltip();
+
+// $('.btn').click(function(){
+//     //Removing `data-toggle` from all elements
+//     $('.btn').removeData('toggle');
+//     //Adding `data-toggle` on clicked element
+//     $(this).data('toggle','button');        
+// });
+
+// $().button('toggle')
+
+/* Latest compiled and minified JavaScript included as External Resource */
+$('.btn-switch').click(function() {
+  $('.btn-switch').removeClass("active red blue");
+  if ($(this).find('span').text() == "Home")
+    $(this).addClass('active red');
+  else
+    $(this).addClass('active blue');
+});
