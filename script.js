791fa23706c4e5cd03ed3707856962cee7bae91b
sanial <sanial369@gmail.com>
2023-09-02 17:43:22 -0400

    #9223-1741 proj sizing, centering

--- script.js
+++ script.js
@@ -9,20 +9,61 @@ var scrollSpy = new bootstrap.ScrollSpy(document.body, {
   target: '#navbar-example'
 })
 
-// $('.btn').click(function(){
-//     //Removing `data-toggle` from all elements
-//     $('.btn').removeData('toggle');
-//     //Adding `data-toggle` on clicked element
-//     $(this).data('toggle','button');        
-// });
 
-// $().button('toggle')
+/**Topographic background
+// Topographic map animation by @jonyablonski Code Pen
+**/
+var svgContainer = document.querySelector('#svg-container');
+var svgElem = document.querySelector('#svg-container svg');
+var path = document.querySelectorAll('.line');
+var svgWOrig;
+var svgHOrig;
+svgWOrig = svgElem.getAttribute('width');
+svgHOrig = svgElem.getAttribute('height');
+var minW = 320;
 
-/* Latest compiled and minified JavaScript included as External Resource */
-// $('.btn-switch').click(function() {
-//   $('.btn-switch').removeClass("active red blue");
-//   if ($(this).find('span').text() == "Home")
-//     $(this).addClass('active red');
-//   else
-//     $(this).addClass('active blue');
-// });
+for(var i = 0; i < path.length; i++) {
+	var length = path[i].getTotalLength();
+	path[i].style.transition = path[i].style.WebkitTransition = 'none';
+	path[i].style.strokeDasharray = length + ' ' + length;
+	path[i].style.strokeDashoffset = length;
+	path[i].getBoundingClientRect();
+	path[i].style.transition = path[i].style.WebkitTransition = 'stroke-dashoffset 10s ease';
+	path[i].style.strokeDashoffset = '0';
+}
+
+var svgCover = function() {
+
+    // Find the current width and height of the viewport
+    var winWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    var winHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+    // Resize the svg container to match the viewport
+    svgContainer.style.width = winWidth + 'px';
+    svgContainer.style.height = winHeight + 'px';
+
+    // Find the largest scale factor of horizontal/vertical
+    var scaleH = winWidth / svgWOrig;
+    var scaleV = winHeight / svgHOrig;
+    var scale = scaleH > scaleV ? scaleH : scaleV;
+
+    // Don't allow scaled width to be less than min width
+    if (scale * svgWOrig < minW) {
+        scale = minW / svgWOrig;
+    }
+
+    // Scale the svg
+    var svgNewWidth = scale * svgWOrig;
+    var svgNewHeight = scale * svgHOrig;
+    svgElem.style.width = svgNewWidth + 'px';
+    svgElem.style.height = svgNewHeight + 'px';
+
+    // Align to middle by scrolling within the container
+    svgContainer.scrollLeft = (svgNewWidth - winWidth) / 2;
+    svgContainer.scrollTop = (svgNewHeight - winHeight) / 2;
+  
+};
+
+svgCover();
+
+window.addEventListener('resize', svgCover);
