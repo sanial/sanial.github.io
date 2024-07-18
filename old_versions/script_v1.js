8e14c342121ac2730af0d993690d0bac0d18d6f8
sanial <sanial369@gmail.com>
2024-07-18 19:34:59 -0400

    #718730 new ui!!! git update

---
+++ old_versions/script_v1.js
@@ -0,0 +1,112 @@
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+$('[data-toggle="tooltip"]').tooltip();
+
+var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+  target: '#navbar-example'
+})
+
+
+/**Topographic background
+// Topographic map animation by @jonyablonski Code Pen
+**/
+var svgContainer = document.querySelector('#svg-container');
+var svgElem = document.querySelector('#svg-container svg');
+var path = document.querySelectorAll('.line');
+var svgWOrig;
+var svgHOrig;
+svgWOrig = svgElem.getAttribute('width');
+svgHOrig = svgElem.getAttribute('height');
+var minW = 320;
+
+for(var i = 0; i < path.length; i++) {
+	var length = path[i].getTotalLength();
+	path[i].style.transition = path[i].style.WebkitTransition = 'none';
+	path[i].style.strokeDasharray = length + ' ' + length;
+	path[i].style.strokeDashoffset = length;
+	path[i].getBoundingClientRect();
+	path[i].style.transition = path[i].style.WebkitTransition = 'stroke-dashoffset 10s ease';
+	path[i].style.strokeDashoffset = '0';
+}
+
+var svgCover = function() {
+
+    // Find the current width and height of the viewport
+    var winWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    var winHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+    // Resize the svg container to match the viewport
+    svgContainer.style.width = winWidth + 'px';
+    svgContainer.style.height = winHeight + 'px';
+
+    // Find the largest scale factor of horizontal/vertical
+    var scaleH = winWidth / svgWOrig;
+    var scaleV = winHeight / svgHOrig;
+    var scale = scaleH > scaleV ? scaleH : scaleV;
+
+    // Don't allow scaled width to be less than min width
+    if (scale * svgWOrig < minW) {
+        scale = minW / svgWOrig;
+    }
+
+    // Scale the svg
+    var svgNewWidth = scale * svgWOrig;
+    var svgNewHeight = scale * svgHOrig;
+    svgElem.style.width = svgNewWidth + 'px';
+    svgElem.style.height = svgNewHeight + 'px';
+
+    // Align to middle by scrolling within the container
+    svgContainer.scrollLeft = (svgNewWidth - winWidth) / 2;
+    svgContainer.scrollTop = (svgNewHeight - winHeight) / 2;
+  
+};
+
+svgCover();
+
+window.addEventListener('resize', svgCover);
+
+//
+/**Fading Scroll */
+//
+
+//Tab Window
+
+// shortcuts 
+
+const log = console.log
+
+// variables
+
+let modalTab = document.querySelector('.tab-modal')
+let modalImg = document.querySelector('.tab-modal__img')
+let closer = document.querySelector('.tab-modal__close')
+
+let tabs_img = document.getElementsByClassName('tab-img')
+
+// functions 
+
+function defaultImage(){
+    modalImg.src = tabs_img[0].src
+    modalImg.alt = tabs_img[0].alt
+    modalTab.classList.add('show')
+}
+
+// events
+
+defaultImage()
+
+closer.addEventListener('click', () => {
+    modalTab.classList.remove('show')
+})
+
+for(let i=0; i < tabs_img.length; i++){
+    tabs_img[i].addEventListener('click', () => {
+        modalImg.src = tabs_img[i].src
+        modalImg.alt = tabs_img[i].alt
+        modalTab.classList.add('show')
+    })
+}
+
